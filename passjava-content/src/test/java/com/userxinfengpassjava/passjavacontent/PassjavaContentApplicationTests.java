640541792049edceb0e2a6039a69e2f141d2c2b0
userXinFeng <2247518830@qq.com>
2022-03-15 17:25:02 +0800

    2022-3-15

---
+++ passjava-content/src/test/java/com/userxinfengpassjava/passjavacontent/PassjavaContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userxinfengpassjava.passjavacontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaContentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
